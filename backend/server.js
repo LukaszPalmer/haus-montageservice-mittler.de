c6212c3cee2e80689cc8ff1956c4e8993e9c89e4
LukaszPalmer <lukaszstanislawpalmer95@gmail.com>
2026-03-16 18:51:57 +0100

    New ES6 Syntax IMPORT

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const nodemailer = require("nodemailer");
-const multer = require("multer");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import nodemailer from "nodemailer";
+import multer from "multer";
 
 dotenv.config();
 
